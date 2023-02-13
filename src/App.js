20cb13e2828bff4d2eaeed5e26628ca0b8ffd44c
Atul Kumar <37495003+Atulhackit@users.noreply.github.com>
2023-02-13 15:13:09 +0530

    added another select

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
 import "./styles.css";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [country, setCountry] = useState("");
+  const [selectedCities, setCities] = useState([]);
   const countries = [
-    { name: "india", value: "IN", cities: ["Delhi", "munbai", "Noida"] },
+    { id: 0, name: "india", value: "IN", cities: ["Delhi", "munbai", "Noida"] },
     {
+      id: 1,
       name: "Pakistan",
       value: "pak",
       cities: ["Karachi", "Lahor", "islamanbad"]
     },
-    { name: "China", value: "chi", cities: ["wohang", "bezing", "chingchang"] }
+    {
+      id: 2,
+      name: "China",
+      value: "chi",
+      cities: ["wohang", "bezing", "chingchang"]
+    }
   ];
+  console.log(countries[country]);
+
+  useEffect(() => {
+    countries.filter((item) => {
+      if (item.name === country) {
+        setCities(item.cities);
+      }
+    });
+  }, [country]);
+  console.log(selectedCities);
   return (
     <div className="App">
       <h1>MultiLayer DropDown</h1>
-      <select value="select cities">
+      <select value={country} onChange={(e) => setCountry(e.target.value)}>
         {countries.map((country) => {
-          return <option>{country}</option>;
+          return (
+            <option key={country?.id} value={country.name}>
+              {country.name}
+            </option>
+          );
+        })}
+      </select>
+      <select>
+        {selectedCities.map((city) => {
+          return <option value={city}>{city}</option>;
         })}
       </select>
     </div>
